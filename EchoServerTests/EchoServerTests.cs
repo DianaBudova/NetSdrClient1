498bac94ee1a46fb4148cc0db2896ef287260b9d
Budova Diana <dianachampions011@gmail.com>
2025-10-12 14:23:23 +0300

    Tests Covered EchoServer

--- EchoServerTests/EchoServerTests.cs
+++ EchoServerTests/EchoServerTests.cs
@@ -3,9 +3,9 @@ using Moq;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Text;
-using System.Timers;
-using EchoServer;
 using EchoServer.Abstractions;
+using System;
+using System.IO;
 
 namespace EchoServerTests
 {
@@ -27,68 +27,61 @@ namespace EchoServerTests
         [Test]
         public async Task StartAsync_ShouldStartListenerAndAcceptClients()
         {
-            // Arrange
             var mockClient = new Mock<ITcpClient>();
             var mockStream = new Mock<INetworkStream>();
-
             mockClient.Setup(c => c.GetStream()).Returns(mockStream.Object);
 
             _mockListener.SetupSequence(l => l.AcceptTcpClientAsync())
                          .ReturnsAsync(mockClient.Object)
                          .ThrowsAsync(new OperationCanceledException());
 
-            // Act
             await _server.StartAsync();
 
-            // Assert
             _mockListener.Verify(l => l.Start(), Times.Once);
             _mockListener.Verify(l => l.AcceptTcpClientAsync(), Times.Exactly(2));
             _mockLogger.Verify(log => log.Log("Server started."), Times.Once);
+            _mockLogger.Verify(log => log.Log("Server shutdown."), Times.Once);
         }
-
-        //[Test]
-        //public async Task HandleClientAsync_ShouldEchoReceivedData()
-        //{
-        //    // Arrange
-        //    var mockClient = new Mock<ITcpClient>();
-        //    var mockStream = new Mock<INetworkStream>();
-        //    var message = "Hello, World!";
-        //    var messageBytes = Encoding.UTF8.GetBytes(message);
-        //    var buffer = new byte[1024];
-
-        //    mockStream.SetupSequence(s => s.ReadAsync(It.IsAny<byte[]>(), 0, It.IsAny<int>(), It.IsAny<CancellationToken>()))
-        //        .Returns((byte[] b, int o, int s, CancellationToken t) =>
-        //        {
-        //            messageBytes.CopyTo(b, o);
-        //            return Task.FromResult(messageBytes.Length);
-        //        })
-        //        .ReturnsAsync(0);
-
-        //    mockClient.Setup(c => c.GetStream()).Returns(mockStream.Object);
-
-        //    // Act
-        //    await _server.HandleClientAsync(mockClient.Object, CancellationToken.None);
-
-        //    // Assert
-        //    mockStream.Verify(s => s.WriteAsync(
-        //        It.Is<byte[]>(b => Encoding.UTF8.GetString(b, 0, message.Length) == message),
-        //        0,
-        //        messageBytes.Length,
-        //        It.IsAny<CancellationToken>()), Times.Once);
-
-        //    mockClient.Verify(c => c.Close(), Times.Once);  
-        //    _mockLogger.Verify(log => log.Log($"Echoed {messageBytes.Length} bytes to the client."), Times.Once);
-        //}
 
         [Test]
         public void Stop_ShouldStopListenerAndCancelToken()
         {
-            // Act
             _server.Stop();
 
-            // Assert
             _mockListener.Verify(l => l.Stop(), Times.Once);
             _mockLogger.Verify(log => log.Log("Server stopped."), Times.Once);
+        }
+
+        [Test]
+        public async Task HandleClientAsync_ShouldLogErrorAndCloseClient_WhenStreamThrowsException()
+        {
+            var mockClient = new Mock<ITcpClient>();
+            var mockStream = new Mock<INetworkStream>();
+            var exceptionMessage = "Connection was forcibly closed.";
+
+            mockClient.Setup(c => c.GetStream()).Returns(mockStream.Object);
+
+            mockStream.Setup(s => s.ReadAsync(It.IsAny<byte[]>(), 0, It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                      .ThrowsAsync(new IOException(exceptionMessage));
+
+            await _server.HandleClientAsync(mockClient.Object, CancellationToken.None);
+
+            mockStream.Verify(s => s.WriteAsync(It.IsAny<byte[]>(), 0, It.IsAny<int>(), It.IsAny<CancellationToken>()), Times.Never);
+            _mockLogger.Verify(log => log.Log($"Error: {exceptionMessage}"), Times.Once);
+            mockClient.Verify(c => c.Close(), Times.Once);
+            _mockLogger.Verify(log => log.Log("Client disconnected."), Times.Once);
+        }
+
+        [Test]
+        public async Task StartAsync_ShouldStopGracefully_WhenListenerThrowsObjectDisposedException()
+        {
+            _mockListener.Setup(l => l.AcceptTcpClientAsync()).ThrowsAsync(new ObjectDisposedException("TcpListener"));
+
+            await _server.StartAsync();
+
+            _mockListener.Verify(l => l.Start(), Times.Once);
+            _mockLogger.Verify(log => log.Log("Server started."), Times.Once);
+            _mockLogger.Verify(log => log.Log("Server shutdown."), Times.Once);
         }
     }
 }
